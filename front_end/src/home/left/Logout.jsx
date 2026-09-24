9b691997a42eb34f0915f9a79ceb186c6599fc1e
sankhadipjana <sankhadipjana31@gmail.com>
2026-09-24 19:40:54 +0530

    logeout functionality

--- front_end/src/home/left/Logout.jsx
+++ front_end/src/home/left/Logout.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import { TbLogout2 } from "react-icons/tb";
+import axios from 'axios';
+import cookie from 'js-cookie';
+import  toast  from 'react-hot-toast';
 
 
 const Logout = () => {
+  const [loading, setLoading] = React.useState(false);
+  const handleLogout = async ()=>{
+   setLoading(true);
+    try {
+      const res= await axios.post("http://localhost:3000/api/users/logout");
+      localStorage.removeItem("user");
+      cookie.remove("jwt");
+      setLoading(false);
+      window.location.reload();
+      alert("Logout successful");
+    }catch(error){
+      console.log(error)
+      setLoading(false);
+    }
+  }
   return (
     <div>
-        <TbLogout2 className='text-5xl text-white hover:text-sky-400 pb-6 m-1 rounded-3xl' />
+        <TbLogout2 className='text-5xl text-white hover:text-sky-400 pb-6 m-1 rounded-3xl'onClick={handleLogout} />
     </div>
   )
 }
--- front_end/src/main.jsx
+++ front_end/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/authprovider.jsx'
+import { Toaster } from 'react-hot-toast'
 
 import { BrowserRouter } from 'react-router-dom'
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
    <AuthProvider>
      <App />
+     <Toaster/>
    </AuthProvider>
    </BrowserRouter>
   
